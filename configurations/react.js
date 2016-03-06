301f0a204dab51fafd45471c1ca63988e6f93cb4
Brian Di Palma <offler@gmail.com>
2016-03-06 17:49:37 +0000

    feat: Use React ESLint plugin recommended rules as base

--- configurations/react.js
+++ configurations/react.js
@@ -1,19 +1,20 @@
 "use strict";
 
 module.exports = {
-	"extends": "./default.js",
+	"extends": [
+		"./default.js",
+		"plugin:react/recommended"
+	],
 	"plugins": [
 		"react"
 	],
 	"parserOptions": {
 		 "ecmaFeatures": {
-				"jsx": true,
 				"experimentalObjectRestSpread": true
 		}
 	},
 	"rules": {
 		"no-extra-parens": [2, "functions"],
-		"react/display-name": 2,
 		"react/forbid-prop-types": 1,
 		"react/jsx-boolean-value": [1, "always"],
 		"react/jsx-closing-bracket-location": 1,
@@ -24,25 +25,13 @@ module.exports = {
 		"react/jsx-indent": [1, "tab"],
 		"react/jsx-key": 1,
 		"react/jsx-max-props-per-line": 1,
-		"react/jsx-no-duplicate-props": 1,
 		"react/jsx-no-literals": 1,
-		"react/jsx-no-undef": 1,
 		"react/jsx-pascal-case": 1,
 		"react/jsx-sort-props": 1,
 		"react/jsx-space-before-closing": 1,
-		"react/jsx-uses-react": 1,
-		"react/jsx-uses-vars": 1,
-		"react/no-deprecated": 1,
-		"react/no-did-mount-set-state": 1,
-		"react/no-did-update-set-state": 1,
-		"react/no-direct-mutation-state": 1,
-		"react/no-is-mounted": 1,
 		"react/no-multi-comp": 1,
 		"react/no-string-refs": 1,
-		"react/no-unknown-property": 1,
 		"react/prefer-es6-class": 1,
-		"react/prop-types": 1,
-		"react/react-in-jsx-scope": 1,
 		"react/self-closing-comp": 1,
 		"react/sort-comp": 1,
 		"react/sort-prop-types": 1,
